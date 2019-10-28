ff7e2a815235bd3a830d7c7a88a70ec2164f6469
Rebecca.Lynn.Fletcher@gmail.com <thefletcher@gmail.com>
2019-10-28 14:37:58 -0500

    initial add/edit blog post pages

--- blog/src/main/java/com/sg/blog/controller/ContentController.java
+++ blog/src/main/java/com/sg/blog/controller/ContentController.java
@@ -20,6 +20,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -43,10 +44,16 @@ public class ContentController {
     public String displayContentPage() {
         return "content";
     }
+    
+    @GetMapping("addContent")
+    public String addContent(Model model) {
+        model.addAttribute("blogpost", new BlogPost());
+        return "addContent";
+    }
 
     @PostMapping("addContent")
-    public String addContent(HttpServletRequest request, Principal principal) {
-        String content = request.getParameter("mytextarea");
+    public String performAddContent(HttpServletRequest request, Principal principal) {
+        String content = request.getParameter("blogtextarea");
         BlogPost blogPost = new BlogPost();
         blogPost.setBody(content);
         String title = request.getParameter("title");
@@ -71,6 +78,50 @@ public class ContentController {
         blogPost.setApproved(request.isUserInRole("ROLE_ADMIN"));
         User user = userDao.findByUsername(principal.getName());
         blogPost.setUser(user);
+        blogPost = blogDao.save(blogPost);
+        return "redirect:/home";
+    }
+    
+    @GetMapping("editContent")
+    public String editContent(Integer id, Model model) {
+        BlogPost blogPost = blogDao.findById(id).orElse(null);
+        model.addAttribute("blogpost", blogPost);
+        List<String> tags = new ArrayList<>();
+        for(Tag tag: blogPost.getTags()){
+            tags.add(tag.getName());
+        }
+        model.addAttribute("tags", tags);
+        return "editContent";
+    }
+
+    @PostMapping("editContent")
+    public String performEditContent(Integer id, HttpServletRequest request) {
+        String content = request.getParameter("blogtextarea");
+        BlogPost blogPost = blogDao.findById(id).orElse(null);
+        blogPost.setBody(content);
+        String title = request.getParameter("title");
+        blogPost.setTitle(title);
+        String rawTags = request.getParameter("tags");
+        String[] tagsArray = rawTags.split(" ");
+        List<Tag> tags = new ArrayList<>();
+        for (String tagName : tagsArray) {
+            Tag tag = tagDao.findByName(tagName);
+            if (tag == null) {
+                tag = new Tag();
+                tag.setName(tagName);
+                tag = tagDao.save(tag);
+            }
+            tags.add(tag);
+        }
+        blogPost.setTags(tags);
+        String updateDate = request.getParameter("updateDate");
+        if(Boolean.parseBoolean(updateDate)){
+            blogPost.setDate(LocalDateTime.now().withNano(0));
+        }
+        if (request.getParameter("expDate") != null) {
+            blogPost.setExpirationDate(LocalDate.parse(request.getParameter("expDate"), DateTimeFormatter.ISO_DATE));
+        }
+        blogPost.setApproved(request.isUserInRole("ROLE_ADMIN"));
         blogPost = blogDao.save(blogPost);
         return "redirect:/home";
     }
