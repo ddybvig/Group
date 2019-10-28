f60c04e5c6ea62a2d838ab3456b0d5b40a0e78dd
Dale Dybvig <dalerdybvig@gmail.com>
2019-10-28 15:57:15 -0500

    added some formatting to home page, as well as delete and edit links

--- blog/src/main/java/com/sg/blog/controller/AdminController.java
+++ blog/src/main/java/com/sg/blog/controller/AdminController.java
@@ -24,89 +24,90 @@ import org.springframework.web.bind.annotation.PostMapping;
  */
 @Controller
 public class AdminController {
+
     @Autowired
     RoleDao roles;
-    
+
     @Autowired
     UserDao users;
-    
+
     @Autowired
     PasswordEncoder encoder;
-    
-    
-    @GetMapping ("/admin")
-    public String diplayAdminPage(){
-        return "admin";    
+
+    @GetMapping("/admin")
+    public String diplayAdminPage() {
+        return "admin";
     }
-    
-      @PostMapping("/addUser")
+
+    @PostMapping("/addUser")
     public String addUser(String username, String password) {
         User user = new User();
         user.setUsername(username);
         user.setPassword(encoder.encode(password));
         user.setEnabled(true);
-        
+
         List<Role> userRoles = new ArrayList<>();
         userRoles.add(roles.findByRole("ROLE_USER"));
         user.setRoles(userRoles);
-        
+
         users.save(user);
-        
-        return "redirect:/admin"; 
+
+        return "redirect:/admin";
     }
-    
-     @GetMapping("/deleteUser")
+
+    @GetMapping("/deleteUser")
     public String deleteUser(Integer id) {
         users.deleteById(id);
         return "redirect:/admin";
     }
-     
+
     @GetMapping("/editUser")
     public String editUserDisplay(Model model, Integer id, Integer error) {
         User user = users.findById(id).orElse(null);
         List<Role> roleList = roles.findAll();
-        
+
         model.addAttribute("user", user);
         model.addAttribute("roles", roleList);
-        
-         if(error != null) {
-            if(error == 1) {
+
+        if (error != null) {
+            if (error == 1) {
                 model.addAttribute("error", "Passwords did not match, password was not updated.");
             }
         }
         return "editUser";
     }
-    
-    @PostMapping(value="/editUser")
+
+    @PostMapping(value = "/editUser")
     public String editUserAction(String[] roleIdList, Boolean enabled, Integer id) {
         User user = users.findById(id).orElse(null);
-        if(enabled != null) {
+        if (enabled != null) {
             user.setEnabled(enabled);
         } else {
             user.setEnabled(false);
         }
-        
+
         List<Role> roleList = new ArrayList<>();
-        for(String roleId : roleIdList) {
+        for (String roleId : roleIdList) {
             Role role = roles.findById(Integer.parseInt(roleId)).orElse(null);
             roleList.add(role);
         }
         user.setRoles(roleList);
         users.save(user);
-         
+
         return "redirect:/admin";
     }
-     @PostMapping("editPassword") 
+
+    @PostMapping("editPassword")
     public String editPassword(Integer id, String password, String confirmPassword) {
         User user = users.findById(id).orElse(null);
-        
-        if(password.equals(confirmPassword)) {
+
+        if (password.equals(confirmPassword)) {
             user.setPassword(encoder.encode(password));
             users.save(user);
             return "redirect:/admin";
         } else {
             return "redirect:/editUser?id=" + id + "&error=1";
         }
-     
+
     }
 }
