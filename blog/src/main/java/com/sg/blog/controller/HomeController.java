88d54d028335c00001a8a1dcb69240a9b26c90b5
Dale Dybvig <dalerdybvig@gmail.com>
2019-10-29 15:57:21 -0500

    got the search by tag mostly working yay.

--- blog/src/main/java/com/sg/blog/controller/HomeController.java
+++ blog/src/main/java/com/sg/blog/controller/HomeController.java
@@ -20,26 +20,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
  * @author MARIA
  */
-@Controller 
+@Controller
 public class HomeController {
-    
+
     @Autowired
     BlogPostDao blogDao;
-    
+
     @Autowired
     TagDao tagDao;
-    
+
     @Autowired
     StaticPageDao staticDao;
-    
-    String searchInput;
-    
+
     @GetMapping({"/", "/home"})
     public String displayHomePage(Model model) {
         List<BlogPost> posts = blogDao.findAll();
@@ -54,36 +51,43 @@ public class HomeController {
         model.addAttribute("tagMap", tagMap);
         return "home";
     }
-    
-    @PostMapping("searchByTag")
-    public String searchByTag(HttpServletRequest request) {
-        searchInput = request.getParameter("searchInput");
-        
-        return "searchResults";
-    }
-    
+
     @GetMapping("searchResults")
-    public String getSearchResults(Model model) {
+    public String getSearchResults(HttpServletRequest request, Model model) {
+        String searchInput = request.getParameter("searchInput");
         List<Tag> tags = tagDao.findAll();
         Tag tagFromSearch = null;
         for (Tag someTag : tags) {
             if (someTag.getName().equalsIgnoreCase(searchInput)) {
                 tagFromSearch = someTag;
-            } else {
-                return "redirect:/"; //or maybe make an error and display it on the page?
             }
         }
-        Integer tagId = tagFromSearch.getId();
-        List<BlogPost> posts = new ArrayList<>();
-        for (BlogPost post : blogDao.findAll()) {
-            for (Tag tag : post.getTags()) {
-                if (tag.getId()==tagId) {
-                    posts.add(post);
+        if (tagFromSearch != null) {
+            Integer tagId = tagFromSearch.getId();
+            List<BlogPost> posts = new ArrayList<>();
+            for (BlogPost post : blogDao.findAll()) {
+                for (Tag tag : post.getTags()) {
+                    if (tag.getId() == tagId) {
+                        posts.add(post);
+                    }
                 }
             }
+            model.addAttribute("posts", posts);
+            return "searchResults";
+        } else {
+            String searchErrorMessage = "Tag not found. Please check spelling and verify there are no extra spaces or characters, or search for a new tag.";
+            model.addAttribute("searchErrorMessage", searchErrorMessage);
+            List<BlogPost> posts = blogDao.findAll();
+            model.addAttribute("posts", posts);
+            Map<Integer, List<String>> tagMap = new HashMap<>();
+            for (BlogPost post : posts) {
+                List<Tag> tagsReload = post.getTags();
+                tagMap.put(post.getId(), post.getTags().stream()
+                        .map(t -> t.getName())
+                        .collect(Collectors.toList()));
+            }
+            model.addAttribute("tagMap", tagMap);
+            return "home";
         }
-        model.addAttribute("posts", posts);
-        return "searchResults";
     }
-    
 }
